50aa97ea5977810cf02a4ebc7b44b6051a949c45
Christopher André <chan19af@student.cbs.dk>
2021-10-10 09:59:47 +0200

    Added code from ovelse 5 + comments from teachers

--- App.js
+++ App.js
@@ -1,21 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import firebase from "firebase";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import CarList from "./Components/CarList";
+import Add_edit_Car from "./Components/Add_edit_Car";
+import CarDetails from "./Components/CarDetails";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 export default function App() {
+  const Stack = createStackNavigator();
+  const Tab = createBottomTabNavigator();
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAebRD5_5teSCI7MS2t9TqqS0Gj5cWbFbQ",
+    authDomain: "cardatabase-8b1e3.firebaseapp.com",
+    projectId: "cardatabase-8b1e3",
+    storageBucket: "cardatabase-8b1e3.appspot.com",
+    messagingSenderId: "386678222680",
+    appId: "1:386678222680:web:943e8d3ec2e7949b7c7f2d",
+  };
+
+  // Vi kontrollerer at der ikke allerede er en initialiseret instans af firebase
+  // Så undgår vi fejlen Firebase App named '[DEFAULT]' already exists (app/duplicate-app).
+
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  const StackNavigation = () => {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen name={"Car List"} component={CarList} />
+        <Stack.Screen name={"Car Details"} component={CarDetails} />
+        <Stack.Screen name={"Edit Car"} component={Add_edit_Car} />
+      </Stack.Navigator>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name={"Home"}
+          component={StackNavigation}
+          options={{
+            tabBarIcon: () => <Ionicons name="home" size={20} />,
+            headerShown: null,
+          }}
+        />
+        <Tab.Screen
+          name={"Add"}
+          component={Add_edit_Car}
+          options={{ tabBarIcon: () => <Ionicons name="add" size={20} /> }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Components/CarListIteam.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+const CarListIteams = (props) => {
+  return <Text>Det er mit CarListIteams</Text>;
+};
+
+export default CarListIteams;
